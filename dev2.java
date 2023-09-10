15e1417aa87be65b7b2fbf02738fca0403165b4b
PrithvirajGorule <prithvi,gorule@gmail.com>
2023-09-10 11:21:27 +0530

    dev2

---
+++ dev2.java
@@ -0,0 +1,12 @@
+class a2
+{ public static void main(String args[])
+    {
+    System.out.println("hello world");
+    System.out.println("hello world");
+    System.out.println("hello helllooo");
+        System.out.println("this is 1");
+    System.out.println("this is  2");
+    System.out.println("this is 3");
+
+    }
+}
